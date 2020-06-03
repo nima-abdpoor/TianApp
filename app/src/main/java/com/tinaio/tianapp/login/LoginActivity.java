bc4fee7bfff9c9e2c2d38cd484d293b395c06e4f
nimaabdpoor <nimaabdi328@yahoo.com>
2020-06-03 20:22:40 +0430

    login page UI added.

---
+++ app/src/main/java/com/tinaio/tianapp/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.tinaio.tianapp.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tinaio.tianapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
